2743f132e9be29dba1285310c5f3a57df2054a07
Fernando <fernando.decalle@gmail.com>
2023-03-11 10:03:57 +0100

    algo

--- src/app.py
+++ src/app.py
@@ -4,12 +4,11 @@ from eq_w import EqwAlgo
 
 def handler(event, context):
     # Meteis el código completo del algo----->>>>
-    # ah = BMEApiHandler()
-    # df_san = ah.get_ohlcv_data('SAN')
-    # print(df_san)
+    ah = BMEApiHandler()
+    df_san = ah.get_ohlcv_data('SAN')
+    print(df_san)
 
-    # # Llamar al algo
-    # eq_w_1 = EqwAlgo(algo_tag='test_user_1_miax10_algo3', n_days=10)
-    # eq_w_1.run_day_comp()
-    print("HOLA")
+    # Llamar al algo
+    eq_w_1 = EqwAlgo(algo_tag='test_user_1_miax10_algo3', n_days=10)
+    eq_w_1.run_day_comp()
 
